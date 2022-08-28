74c69785258414bc5664ff74f7bd3ee8dbedff83
mhson-stack <72399227+mhson-stack@users.noreply.github.com>
2022-08-29 00:40:52 +0900

    change time clock.py

--- clock.py
+++ clock.py
@@ -1,17 +1,17 @@
 from database import get_coin_history, import_to_db
 from apscheduler.schedulers.blocking import BlockingScheduler
-from datetime import datetime, date, timedelta
+from datetime import datetime, date, timedelta, timezone
 
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=0, minute=29, timezone='Asia/Seoul')
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=0, minute=42, timezone='Asia/Seoul')
 def scheduled_job():
     print('This job is run every weekday at 5pm.')
     symbols = ["BTC", "ETH", "ETC", "XRP", "BNB"]
-    start = (date.today() - timedelta(days=1)).strftime("%Y-%m-%d") + "T00:00+09:00"
+    start = (datetime.now(timezone.utc) - timedelta(days=1)).strftime("%Y-%m-%dT%H:00")
     print(date.today())
-    end = date.today().strftime("%Y-%m-%d") + "T01:00+09:00"
+    end = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:00")
 
     for symbol in symbols:
         df = get_coin_history(symbol, start=start, end=end)
