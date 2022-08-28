1f08d28c90865e48f53b35c06693f963d2ced21b
mhson-stack <72399227+mhson-stack@users.noreply.github.com>
2022-08-29 01:42:27 +0900

    change time clock.py

--- clock.py
+++ clock.py
@@ -5,18 +5,18 @@ from datetime import datetime, date, timedelta, timezone
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=1, minute=30, timezone='Asia/Seoul')
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=1, minute=44, timezone='Asia/Seoul')
 def scheduled_job():
     print('This job is run every day at 0 am.')
     symbols = ["BTC", "ETH", "ETC", "XRP", "BNB"]
-    start = (datetime.now(timezone.utc) - timedelta(hours=14)).strftime("%Y-%m-%dT01:00+00:00")
-    end = (datetime.now(timezone.utc) + timedelta(hours=9)).strftime("%Y-%m-%dT00:00+00:00")
+    start = (datetime.now(timezone.utc) - timedelta(hours=14)).strftime("%Y-%m-%dT01:00+09:00")
+    end = (datetime.now(timezone.utc) + timedelta(hours=9)).strftime("%Y-%m-%dT00:00+09:00")
     print(datetime.now())
     print(f"From {start} to {end}")
 
     for symbol in symbols:
         df = get_coin_history(symbol, start=start, end=end)
-        print(df.iloc[-1,0])
+        print(df.iloc[0, 0:2], df.iloc[-1, 0:2])
         # import_to_db(symbol, df, method="append")
 
     print("DB Updated")
