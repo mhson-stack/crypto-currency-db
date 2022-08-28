9b5cf333388e7e8d3cfb01f5dfe9a66f563c18ac
mhson-stack <72399227+mhson-stack@users.noreply.github.com>
2022-08-29 02:05:41 +0900

    change time clock.py

--- clock.py
+++ clock.py
@@ -5,7 +5,7 @@ from datetime import datetime, date, timedelta, timezone
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=2, minute=4, timezone='Asia/Seoul')
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=2, minutes=7, timezone='Asia/Seoul')
 def scheduled_job():
     print('This job is run every day at 0 am.')
     symbols = ["BTC", "ETH", "ETC", "XRP", "BNB"]
@@ -16,8 +16,7 @@ def scheduled_job():
 
     for symbol in symbols:
         df = get_coin_history(symbol, start=start, end=end)
-        print(df.iloc[0, 0:2], df.iloc[-1, 0:2])
-        # import_to_db(symbol, df, method="append")
+        import_to_db(symbol, df, method="append")
 
     print("DB Updated")
 
